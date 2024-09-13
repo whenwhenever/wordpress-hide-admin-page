f0e3ce81447717a8b13af604f6d6ed31fc58facc
WW <73618381+whenwhenever@users.noreply.github.com>
2024-09-13 16:12:31 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,22 @@
+/* -------- Hide admin URL -------- */
+function redirect_to_nonexistent_page()
+{
+
+  $new_login =  'replace_with_a_random_string';
+  if (strpos($_SERVER['REQUEST_URI'], $new_login) === false) {
+    wp_safe_redirect(home_url('PageAdminPasIci'), 302);
+    exit();
+  }
+}
+add_action('login_head', 'redirect_to_nonexistent_page');
+
+function redirect_to_actual_login()
+{
+
+  $new_login =  'replace_with_a_random_string';
+  if (parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY) == $new_login && ($_GET['redirect'] !== false)) {
+    wp_safe_redirect(home_url("wp-login.php?$new_login&redirect=false"));
+    exit();
+  }
+}
+add_action('init', 'redirect_to_actual_login');
